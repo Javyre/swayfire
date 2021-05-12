Javyre/swayfire

commit bd94e3ba348cbb427b0145a5a41d5cf50a5568f8

fix(core): Make floating nodes always above tiled
Closes #7

src/core/core.cpp
@@ -876,7 +876,7 @@ void Workspace::insert_floating_node(OwnedNode node) {
     node->set_floating(true);
     node->set_ws(this);
     auto layer = output->workspace->create_sublayer(wf::LAYER_WORKSPACE,
-                                                    wf::SUBLAYER_FLOATING);
+                                                    wf::SUBLAYER_DOCKED_ABOVE);
     node->set_sublayer(layer);
 
     floating_nodes.push_back({std::move(node), layer});

commit 61a7586975163d64b83c2fe62c47a42aab3735a6

fix(core): float nodes not going above other float
This was a regression in bd94e3b

Closes #11

src/core/core.cpp
@@ -87,7 +87,7 @@ void INode::set_floating(bool fl) {
 void INode::set_active() {
     parent->set_active_child(this);
     ws->set_active_node(this);
-    bring_to_front();
+    find_root_parent()->bring_to_front();
 }
 
 void INode::tile_request(const bool tile) {
@@ -771,10 +771,8 @@ void SplitNode::set_sublayer(nonstd::observer_ptr<wf::sublayer_t> sublayer) {
 }
 
 void SplitNode::bring_to_front() {
-    // We just need to bring to front a single view in the sublayer to bring
-    // them all to front.
-    if (!children.empty())
-        children.at(active_child).node->bring_to_front();
+    for (auto &child : children)
+        child.node->bring_to_front();
 }
 
 void SplitNode::set_ws(WorkspaceRef ws) {
@@ -861,10 +859,16 @@ Workspace::Workspace(wf::point_t wsid, wf::geometry_t geo,
     LOGD("ws created with root ", tiled_root.node->to_string());
     active_node = tiled_root.node;
 
+    floating_sublayer = output->workspace->create_sublayer(
+        wf::LAYER_WORKSPACE, wf::SUBLAYER_DOCKED_ABOVE);
+
     output->connect_signal("workarea-changed", &on_workarea_changed);
 }
 
-Workspace::~Workspace() { output->disconnect_signal(&on_workarea_changed); }
+Workspace::~Workspace() {
+    output->disconnect_signal(&on_workarea_changed);
+    output->workspace->destroy_sublayer(floating_sublayer);
+}
 
 void Workspace::set_active_node(Node node) { active_node = node; }
 
@@ -877,11 +881,9 @@ void Workspace::insert_floating_node(OwnedNode node) {
     node->parent = this;
     node->set_floating(true);
     node->set_ws(this);
-    auto layer = output->workspace->create_sublayer(wf::LAYER_WORKSPACE,
-                                                    wf::SUBLAYER_DOCKED_ABOVE);
-    node->set_sublayer(layer);
+    node->set_sublayer(floating_sublayer);
 
-    floating_nodes.push_back({std::move(node), layer});
+    floating_nodes.push_back({std::move(node), floating_sublayer});
 }
 
 Workspace::FloatingNodeIter Workspace::find_floating(Node node) {
@@ -897,7 +899,6 @@ OwnedNode Workspace::remove_floating_node(Node node, bool reset_active) {
     }
 
     auto owned_node = std::move(child->node);
-    output->workspace->destroy_sublayer(child->sublayer);
 
     floating_nodes.erase(child);
 

src/core/core.hpp
@@ -639,6 +639,9 @@ class Workspace : public INodeParent {
     /// The tiled tree that fills this workspace.
     WorkspaceRoot<SplitNode> tiled_root;
 
+    /// The sublayer which holds all floating nodes in this workspace.
+    nonstd::observer_ptr<wf::sublayer_t> floating_sublayer;
+
     /// The floating nodes that are manages by this ws.
     ///
     /// All floating nodes are direct children of their workspace.